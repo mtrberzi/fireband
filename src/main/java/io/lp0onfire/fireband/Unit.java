6e3e8dc18659d8b50d020946c07deda39e175739
Murphy Berzish <murphy.berzish@gmail.com>
2014-08-19 14:00:48 -0500

    unit builder

--- src/main/java/io/lp0onfire/fireband/Unit.java
+++ src/main/java/io/lp0onfire/fireband/Unit.java
@@ -27,12 +27,27 @@ public class Unit {
   
   private int level = 1; 
   public int getLevel(){return level;}
+  // Experience points are not cumulative; any positive quantity is
+  // counted toward the next level, and is reset when a level is gained (or lost).
   private long experience = 0;
   public long getExperience(){return experience;}
   
   // Equipment and inventory.
   private static final int MAX_INVENTORY_SIZE = 25;
   private List<Item> inventory = new ArrayList<Item>();
+  
+  private void processGetItem(Item i){
+    inventory.add(i);
+  }
+  
+  // Silently add an item to inventory (used for setup).
+  public void acquire(Item i){
+    if(inventory.size() == MAX_INVENTORY_SIZE){
+      throw new IllegalStateException("inventory is full");
+    }
+    processGetItem(i);
+  }
+  
   private Weapon equippedWeapon = null;
   private Item equippedArmour_amulet = null;
   private Item equippedArmour_body = null;
@@ -43,4 +58,42 @@ public class Unit {
   private Item equippedArmour_ringL = null;
   private Item equippedArmour_ringR = null;
   private Item equippedArmour_shield = null;
+  
+  private void processEquipWeapon(Weapon w){
+    if(w == null) return;
+    if(equippedWeapon != null){
+      // TODO attempt to remove equipped weapon first
+    }
+    equippedWeapon = w;
+  }
+  
+  // Silently equip an item onto the unit (for setup).
+  public void getEquippedWith(Item i){
+    if(i.canBeWielded()){
+      Weapon w = (Weapon)i;
+      processEquipWeapon(w);
+    }else if(i.canBeWorn()){
+      // TODO processEquipArmour
+    }else{
+      throw new IllegalArgumentException("cannot equip '" + i.getDisplayName() + "'");
+    }
+  }
+  
+  public Unit(String name, Pronoun pronoun,
+      int level, long experience,
+      int baseStrength, int baseDexterity, int baseConstitution,
+      int baseIntelligence, int baseWisdom, int baseCharisma,
+      int baseMovement){
+    this.name = name;
+    this.pronoun = pronoun;
+    this.level = level;
+    this.experience = experience;
+    this.baseStrength = baseStrength;
+    this.baseDexterity = baseDexterity;
+    this.baseConstitution = baseConstitution;
+    this.baseIntelligence = baseIntelligence;
+    this.baseWisdom = baseWisdom;
+    this.baseCharisma = baseCharisma;
+    this.baseMovement = baseMovement;
+  }
 }
---
+++ src/main/java/io/lp0onfire/fireband/UnitBuilder.java
@@ -0,0 +1,113 @@
+package io.lp0onfire.fireband;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UnitBuilder {
+
+  private String name = "Stranger";
+  public void setName(String name){this.name = name;}
+  private Pronoun pronoun = Pronoun.PRONOUN_NEUTRAL;
+  public void setPronoun(Pronoun pronoun){this.pronoun = pronoun;}
+  
+  private int baseStrength = 10;
+  public void setStrength(int a){this.baseStrength = a;}
+  private int baseDexterity = 10;
+  public void setDexterity(int a){this.baseDexterity = a;}
+  private int baseConstitution = 10;
+  public void setConstitution(int a){this.baseConstitution = a;}
+  private int baseIntelligence = 10;
+  public void setIntelligence(int a){this.baseIntelligence = a;}
+  private int baseWisdom = 10;
+  public void setWisdom(int a){this.baseWisdom = a;}
+  private int baseCharisma = 10;
+  public void setCharisma(int a){this.baseCharisma = a;}
+  private int baseMovement = 6;
+  public void setMovement(int a){this.baseMovement = a;}
+  
+  private int level = 1; 
+  public void setLevel(int a){this.level = a;}
+  private long experience = 0;
+  public void setExperience(int a){this.experience = a;}
+  
+  private List<Item> inventory = new ArrayList<Item>();
+  public void addInventoryItem(Item i){this.inventory.add(i);}
+  private Item equippedWeapon = null;
+  private Item equippedArmour_amulet = null;
+  private Item equippedArmour_body = null;
+  private Item equippedArmour_cloak = null;
+  private Item equippedArmour_feet = null;
+  private Item equippedArmour_hands = null;
+  private Item equippedArmour_head = null;
+  private Item equippedArmour_ringL = null;
+  private Item equippedArmour_ringR = null;
+  private Item equippedArmour_shield = null;
+  public void equip(Item i){
+    if(!i.canBeWielded() && !i.canBeWorn()){
+      throw new IllegalArgumentException("cannot equip '" + i.getDisplayName() + "'");
+    }
+    switch(i.getType()) {
+    case TYPE_ARMOUR_AMULET:
+      equippedArmour_amulet = i;
+      break;
+    case TYPE_ARMOUR_BODY:
+      equippedArmour_body = i;
+      break;
+    case TYPE_ARMOUR_CLOAK:
+      equippedArmour_cloak = i;
+      break;
+    case TYPE_ARMOUR_FEET:
+      equippedArmour_feet = i;
+      break;
+    case TYPE_ARMOUR_HANDS:
+      equippedArmour_hands = i;
+      break;
+    case TYPE_ARMOUR_HEAD:
+      equippedArmour_head = i;
+      break;
+    case TYPE_ARMOUR_RING:
+      if(equippedArmour_ringR == null){
+        equippedArmour_ringR = i;
+      }else{
+        equippedArmour_ringL = i;
+      }
+      break;
+    case TYPE_ARMOUR_SHIELD:
+      equippedArmour_shield = i;
+      break;
+    case TYPE_WEAPON:
+      equippedWeapon = i;
+      break;
+    default:
+      throw new IllegalArgumentException("cannot equip '" + i.getDisplayName() + "'"); 
+    }
+  }
+  
+  public UnitBuilder(){
+    
+  }
+  
+  public Unit build(){
+    Unit u = new Unit(name, pronoun, level, experience,
+        baseStrength, baseDexterity, baseConstitution,
+        baseIntelligence, baseWisdom, baseCharisma,
+        baseMovement);
+    // go through inventory
+    for(Item i : inventory){
+      u.acquire(i);
+    }
+    // go through equipment
+    if(equippedWeapon != null) u.getEquippedWith(equippedWeapon);
+    if(equippedArmour_amulet != null) u.getEquippedWith(equippedArmour_amulet);
+    if(equippedArmour_body != null) u.getEquippedWith(equippedArmour_body);
+    if(equippedArmour_cloak != null) u.getEquippedWith(equippedArmour_cloak);
+    if(equippedArmour_feet != null) u.getEquippedWith(equippedArmour_feet);
+    if(equippedArmour_hands != null) u.getEquippedWith(equippedArmour_hands);
+    if(equippedArmour_head != null) u.getEquippedWith(equippedArmour_head);
+    if(equippedArmour_ringR != null) u.getEquippedWith(equippedArmour_ringR);
+    if(equippedArmour_ringL != null) u.getEquippedWith(equippedArmour_ringL);
+    if(equippedArmour_shield != null) u.getEquippedWith(equippedArmour_shield);
+    return u;
+  }
+  
+}
